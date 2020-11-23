a614342182b63163fa341c33438ddf512ec9c912
caichang <chang.cai@quaie.com>
2020-11-23 10:02:42 +0800

    pending

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -370,7 +370,7 @@ class CandidatesController < ApplicationController
     if @category == 'newCallRecord'
       @candidate = Candidate.find(params[:reference_id])
       exp = @candidate.latest_work_experience
-      @call_record = CallRecord.new(name: @candidate.name, phone: @candidate.phone, company: exp.try(:org_cn), title: exp.try(:title))
+      @call_record = CallRecord.new(candidate_id: @candidate.id, name: @candidate.name, phone: @candidate.phone, company: exp.try(:org_cn), title: exp.try(:title))
       @modal_title = t('action.new_model', :model => t('activerecord.models.call_record'))
       @modal_body_form = 'candidates/loading_modal/new_call_record_form'
     end
