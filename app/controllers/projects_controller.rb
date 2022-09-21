92bafa644024224eec30c744443e064397a79ece
chang.cai <zjucc2005@163.com>
2022-09-21 19:21:07 +0800

    pending

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,8 +10,8 @@ class ProjectsController < ApplicationController
     # query = current_user.is_role?('admin') ? Project.all : current_user.projects
     query = params[:my_project] == 'true' ? current_user.projects : Project.all
     query = user_channel_filter(query)
-    query = query.where('created_at >= ?', params[:created_at_ge]) if params[:created_at_ge].present?
-    query = query.where('created_at <= ?', params[:created_at_le]) if params[:created_at_le].present?
+    query = query.where('projects.created_at >= ?', params[:created_at_ge]) if params[:created_at_ge].present?
+    query = query.where('projects.created_at <= ?', params[:created_at_le]) if params[:created_at_le].present?
     %w[name code].each do |field|
       query = query.where("#{field} ILIKE ?", "%#{params[field].strip}%") if params[field].present?
     end
