79974f353f11b51053676e6017f7cec867139758
CC MiPro14 <zjucc2005@163.com>
2024-03-27 17:21:02 +0800

    pending

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -11,7 +11,7 @@ module ProjectsHelper
 
   # project status display style
   def project_status_badge(status)
-    dict = { initialized: 'warning', ongoing: 'success', finished: 'secondary', billing: 'success', billed: 'secondary' }.stringify_keys
+    dict = { initialized: 'warning', ongoing: 'success', finished: 'secondary', billing: 'info', billed: 'secondary' }.stringify_keys
     content_tag :span, Project::STATUS[status] || status, :class => "badge badge-#{dict[status] || 'secondary'}"
   end
 
